3cad80945161077dc8ed33315b41517fde503213
TC-X <92334384+TC-X@users.noreply.github.com>
2023-10-02 10:12:06 -0600

    feat: added code comments

--- src/woocommerce.php
+++ src/woocommerce.php
@@ -1,11 +1,14 @@
 <?php
+// Declare a namespace for this file
 namespace Roots;
 
+// Check if WooCommerce is defined by looking for the WC_ABSPATH constant
 if (defined('WC_ABSPATH')) {
 
     /**
      * Set WooCommerce Theme Support
      */
+    // After the theme is set up, add support for WooCommerce
     add_action('after_setup_theme', function () {
         add_theme_support('woocommerce');
     });
@@ -13,6 +16,7 @@ if (defined('WC_ABSPATH')) {
     /**
      * Set template path to /woocommerce
      */
+    // Alter the WooCommerce template path to point to the /woocommerce directory
     add_filter(
         'woocommerce_template_path',
         function () {
@@ -27,13 +31,15 @@ if (defined('WC_ABSPATH')) {
      *
      * @return string
      */
+    // Custom template loader function for WooCommerce
     function wc_template_loader(string $template)
     {
-
-        if (strpos($template, WC_ABSPATH) === - 1) {
+        // If the template path doesn't end with WC_ABSPATH, return the original template
+        if (strpos($template, WC_ABSPATH) === -1) {
             return $template;
         }
 
+        // Locate and return the new template based on provided logic
         return locate_template(
             app('sage.finder')->locate(
                 WC()->template_path() . str_replace(
@@ -45,25 +51,29 @@ if (defined('WC_ABSPATH')) {
         ) ?: $template;
     }
 
+    // Apply the custom template loader to the main template and comments template
     add_filter('template_include', __NAMESPACE__ . '\\wc_template_loader', 90, 1);
     add_filter('comments_template', __NAMESPACE__ . '\\wc_template_loader', 100, 1);
 
+    // Modify WooCommerce's default template part behavior
     add_filter(
         'wc_get_template_part',
         function ($template) {
+            // Locate the theme template
             $theme_template = locate_template(
                 app('sage.finder')->locate(
                     WC()->template_path() . str_replace(WC_ABSPATH . 'templates/', '', $template)
                 )
             );
 
+            // If theme template exists, modify its output
             if ($theme_template) {
                 $view = app('view.finder')
                     ->getPossibleViewNameFromPath($file = realpath($theme_template));
 
                 $view = trim($view, '\\/.');
 
-                /** Gather data to be passed to view */
+                // Gather and reduce data to be passed to the view
                 $data = array_reduce(
                     get_body_class(),
                     function ($data, $class) use ($view, $file) {
@@ -72,30 +82,33 @@ if (defined('WC_ABSPATH')) {
                     array()
                 );
 
+                // Render the view with the collected data
                 echo view($view, $data)->render();
-                
-                // Return empty string to prevent wc_get_template_part() to add output
+
+                // Prevent the default WooCommerce behavior by returning an empty string
                 return '';
             } else {
-	            return $template;
+                return $template;
             }
         },
         PHP_INT_MAX,
         1
     );
 
+    // Modify the WooCommerce output before a template part is rendered
     add_action(
         'woocommerce_before_template_part',
         function ($template_name, $template_path, $located, $args) {
             $theme_template = locate_template(app('sage.finder')->locate(WC()->template_path() . $template_name));
 
+            // If a matching theme template exists, modify its output
             if ($theme_template) {
                 $view = app('view.finder')
                     ->getPossibleViewNameFromPath($file = realpath($theme_template));
 
                 $view = trim($view, '\\/.');
 
-                /** Gather data to be passed to view */
+                // Gather and reduce data to be passed to the view
                 $data = array_reduce(
                     get_body_class(),
                     function ($data, $class) use ($view, $file) {
@@ -104,6 +117,7 @@ if (defined('WC_ABSPATH')) {
                     array()
                 );
 
+                // Merge the data and render the view
                 echo view(
                     $view,
                     array_merge(
@@ -118,23 +132,24 @@ if (defined('WC_ABSPATH')) {
         4
     );
 
+    // Alter WooCommerce's default template behavior
     add_filter(
         'wc_get_template',
         function ($template, $template_name, $args) {
             $theme_template = locate_template(app('sage.finder')->locate(WC()->template_path() . $template_name));
 
-            // return theme filename for status screen
+            // Check if we're on the WooCommerce status screen in WP admin
             if (
                 is_admin() &&
-                 ! wp_doing_ajax() &&
-                 function_exists('get_current_screen') &&
-                 get_current_screen() &&
-                 get_current_screen()->id === 'woocommerce_page_wc-status'
+                !wp_doing_ajax() &&
+                function_exists('get_current_screen') &&
+                get_current_screen() &&
+                get_current_screen()->id === 'woocommerce_page_wc-status'
             ) {
                 return $theme_template ?: $template;
             }
 
-            // return empty file, output already rendered by 'woocommerce_before_template_part' hook
+            // For other admin screens, render output via the 'woocommerce_before_template_part' hook
             return $theme_template ? view('empty')->getPath() : $template;
         },
         100,
